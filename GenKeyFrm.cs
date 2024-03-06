02bb64ee9902598a8152873b5298117b9d6e38f6
SafarSoFar <133248217+SafarSoFar@users.noreply.github.com>
2024-03-06 14:24:23 +0500

    Update GenKeyFrm.cs

--- GenKeyFrm.cs
+++ GenKeyFrm.cs
@@ -20,12 +20,13 @@ namespace AES__enc_GUI
             this.frm1 = frm1;
             InitializeComponent();
         }
-
+        // Создание ключа на нажатие кнопки
         private void genKeyButton_Click(object sender, EventArgs e)
         {
             this.frm1.aes.GenerateKey();
             keyTextBox.Text = Convert.ToBase64String(this.frm1.aes.Key);
         }
+        // Создание вектора на нажатие кнопки
         private void VIGenButton_Click(object sender, EventArgs e)
         {
             this.frm1.aes.GenerateIV();
@@ -34,38 +35,31 @@ namespace AES__enc_GUI
 
         private void confirmButton_Click(object sender, EventArgs e)
         {
+            //Если ключи пустой - ошибка
             if (keyTextBox.Text == "")
             {
                 MessageBox.Show("A key was not generated!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
+            //Выбор локации где сохранить ключ
             MessageBox.Show("Choose the location where to store the key");
             SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-
+            // Два формата - Все, .txt
             saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
             saveFileDialog1.FilterIndex = 2;
             saveFileDialog1.RestoreDirectory = true;
-
+            // Если все в порядке - создается файл ключа
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                //if ((myStream = saveFileDialog1.OpenFile()) != null)
-                //{
-                // Code to write the stream goes here.
+                // Запись данных ключа
                 string text = "*DO NOT OVERWRITE THE FILE!*\n";
                 text += "Key->";
                 text += keyTextBox.Text + "<-Key\n";
                 text += "Initialization vector->";
                 text += VITextBox.Text + "<-Initialization vector";
-                //byte[] text = Encoding.UTF8.GetBytes("DO NOT OVERWRITE THE FILE!\n Key: ");
-                //byte[] bytes = Encoding.UTF8.GetBytes(text);
-                //myStream.Write(frm1.aes.Key,0,frm1.aes.Key.Length);
-
-                //myStream.Close();
+                
                 File.WriteAllText(saveFileDialog1.FileName, text);
-                /*var attr = File.GetAttributes(saveFileDialog1.FileName);
-                attr = attr | FileAttributes.ReadOnly;
-                File.SetAttributes(saveFileDialog1.FileName, attr);*/
-                //}
+                
                 frm1.EncryptFiles();
                 this.Close();
             }
